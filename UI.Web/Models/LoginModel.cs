4d0b4360517dab3269a77bfcf1ffe0be953c10ba
Sautabh <saurabhrajput142@gmail.com>
2022-02-18 21:55:14 +0530

    Created login page

---
+++ UI.Web/Models/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UI.Web.Models
+{
+    public class LoginModel
+    {
+        /// <summary>
+        /// User Name for login
+        /// </summary>
+        /// 
+        [Required(ErrorMessage = "Please enter user name")]
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Password for login
+        /// </summary>
+        [Required(ErrorMessage = "Please enter password")]
+        public string Password { get; set; }
+    }
+}
